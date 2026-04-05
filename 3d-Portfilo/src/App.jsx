0f41ef36ccaba8b451a9ccce97cac442369f2af5
Pratik-cod <ranepratik13@gmail.com>
2026-04-05 23:16:43 +0530

    Add LogoSection component to display company logos

--- 3d-Portfilo/src/App.jsx
+++ 3d-Portfilo/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './sections/Hero'
 import ShowcaseSection from './components/ShowcaseSection'
 import Navbar from './components/Navbar'
+import LogoSection from './components/LogoSection'
 const App = () => {
   return (
     <>
     <Navbar />
      <Hero />
     <ShowcaseSection />
+    <LogoSection />
     </>
   )
 }
