75d315d285714f3f6bf8fe87b9bfc3e347778771
Pratik-cod <ranepratik13@gmail.com>
2026-04-08 14:22:18 +0530

    Refactor TitleHeader component for improved readability and consistency

--- 3d-Portfilo/src/components/TitleHeader.jsx
+++ 3d-Portfilo/src/components/TitleHeader.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
 
-const TitleHeader = ({title,sub}) => {
+const TitleHeader = ({ title, sub }) => {
   return (
-    <div className='flex flex-col items-center gap-5'>
-      <div className='hero-badge'>
-      <p>{sub}</p>
+    <div className="flex flex-col items-center gap-5">
+      <div className="hero-badge">
+        <p>{sub}</p>
+      </div>
+      <div>
+        <h1 className="font-semibold md:text-5xl text-3xl text-center">
+          {title}
+        </h1>
+      </div>
     </div>
-    <div className='font-semibold md:text-5xl text-3xl text-center'>
-      {title}
-    </div>
-       </div>
-  )
-}
+  );
+};
 
-export default TitleHeader
+export default TitleHeader;
